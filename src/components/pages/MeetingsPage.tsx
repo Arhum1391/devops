123ae66d72d08feb636ee9a654985a79e6134bb4
Arhum Fareed <222407004+Arhum1391@users.noreply.github.com>
2026-05-06 17:45:52 +0500

    Update MeetingsPage.tsx

--- src/components/pages/MeetingsPage.tsx
+++ src/components/pages/MeetingsPage.tsx
@@ -8,7 +8,7 @@ import Navbar from '@/components/Navbar';
 import MinimalBackground from '@/components/MinimalBackground';
 import TechAnimations from '@/components/TechAnimations';
 
-// --- TYPE DEFINITIONs ---
+// --- TYPE DEFINITION ---
 type Meeting = {
   id: number;
   title: string;
